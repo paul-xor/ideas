2db900df88c7f20a649d004ffd36f16c7937f326
Pavel Smolov <palexsmol@gmail.com>
2019-01-15 21:21:17 -0800

    added review functionality

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,13 @@
 class ReviewsController < ApplicationController
-    before_action :require_user
+    before_action :set_review,only:[ :destroy]
+    before_action :require_user, except: [:index, :show]
+    before_action :require_same_user, only: [:destroy]
    
     def create
     @idea = Idea.find(params[:idea_id])
     @review = Review.new(review_params)
     @review.idea = @idea
+    @review.user = current_user
     
         if @review.save
             redirect_to idea_path(@idea.id)
@@ -17,14 +20,23 @@ class ReviewsController < ApplicationController
     def destroy
         @review = Review.find params[:id]
         @review.destroy
-      
+
         redirect_to idea_path(@review.idea)
     end
 
     private
+    def set_review
+        @review = Review.find(params[:id])
+    end
+
     def review_params
         params.require(:review).permit(:body)
     end
 
-    
+    def require_same_user
+        if current_user != @review.user
+            flash[:danger] = "You can edit/delete only your own reviews"
+            redirect_to idea_path(@review.idea)
+        end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_15_182440) do
+ActiveRecord::Schema.define(version: 2019_01_15_203237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,9 @@ ActiveRecord::Schema.define(version: 2019_01_15_182440) do
     t.bigint "idea_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["idea_id"], name: "index_reviews_on_idea_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,4 +42,5 @@ ActiveRecord::Schema.define(version: 2019_01_15_182440) do
   end
 
   add_foreign_key "reviews", "ideas"
+  add_foreign_key "reviews", "users"
 end
